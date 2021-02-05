3190f605f3176784e0a77a761464901fa9d991eb
Fernando Roque <telmo_ferc@hotmail.com>
2021-02-05 15:39:15 +0100

    otros

--- js/page.js
+++ js/page.js
@@ -4,11 +4,15 @@ $(document).ready(function(e){
     var pageSection = $('#page-section').offset().top,
         pageContact=$('#contact-page').offset().top,
         pageHeader=$('#header-page').offset().top,
+        webSite =$('#webSite').offset().top,
         navBar = $('#navbar').height(),
+        pageTeam=$('#team').offset().top,
         contacto=$('#contacto');
         inicio=$('#inicio');
         conocenos = $('#conocenos');
         servicio=$('#servicio');
+        equipo=$('#equipo');
+        btnWebSite = $('#btnWebSite');
     
     
     function scroll() 
@@ -29,18 +33,36 @@ $(document).ready(function(e){
     function scrollPage(pageSection,e){
         $('html,body').animate({
             scrollTop:pageSection
+            
         },500);
         e.preventDefault();
+        $('#collapsibleNavId').css({"display":"none"});
+        $('#navinVisible').css({"display":"none"});
+        $('#navVisible').css({"display":"block"});
+        
     }
 
     document.onscroll=scroll;
 
+    //Manejo de menu Hamburger
+$('#navVisible').on('click',function(e){
+    $('#navVisible').css({"display":"none"});
+    $('#navinVisible').css({"display":"block"});
+    $('#collapsibleNavId').css({"display":"block"});
+});
+
+$('#navinVisible').on("click",function(e){
+    $('#navVisible').css({"display":"block"});
+    $('#navinVisible').css({"display":"none"});
+    $('#collapsibleNavId').css({"display":"none"});
+});
 
     conocenos.on('click',function(e){
         scrollPage((pageSection-navBar),e);
     });
     inicio.on('click',function(e){
         scrollPage((pageHeader),e);
+        //$('#collapsibleNavId').hide();
     });
     contacto.on('click',function(e){
         scrollPage((pageContact-navBar),e);
@@ -48,6 +70,17 @@ $(document).ready(function(e){
     servicio.on('click',function(e){
         scrollPage((pageSection-navBar),e);
     });
+    equipo.on('click',function(e){
+        scrollPage((pageTeam-(navBar+navBar)),e);
+    });
+    btnWebSite.on('click',function(e){
+        //scrollPage((pageSection-navBar),e);
+        //scrollPage((webSite-navbar),e);
+        //alert(webSite);
+        scrollPage(webSite-navBar,e);
+    });
+   
+    
 
 
 });
